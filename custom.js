ae51ef5f01f2438c1abdae9613239e8d3a0e3c0a
AK-Polash <polashk199@gmail.com>
2022-11-13 01:46:34 +0600

    Promise little bit complex & practical example added here

---
+++ custom.js
@@ -0,0 +1,66 @@
+/* =============================================================
+                      Promise live example 
+============================================================= */
+let payment = true;
+let marks = 80;
+let clearence = true;
+
+function enroll() {
+  console.log("A");
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (payment) {
+        resolve("B");
+      } else {
+        reject("Payment Error");
+      }
+    }, 1000);
+  });
+
+  return myPromise;
+}
+
+function beginCourse() {
+  console.log("C");
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (marks >= 70 && marks <= 100) {
+        resolve("D");
+      } else {
+        reject("Failed in Exam");
+      }
+    }, 1000);
+  });
+
+  return myPromise;
+}
+
+function getCertificate() {
+  console.log("E");
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (clearence) {
+        resolve("F");
+      } else {
+        reject("You don't have the Clearence");
+      }
+    }, 1000);
+  });
+
+  return myPromise;
+}
+
+// Consuming the Code:
+enroll()
+  .then((value) => {
+    console.log(value);
+  })
+  .then(beginCourse)
+  .then((value) => {
+    console.log(value);
+  })
+  .then(getCertificate)
+  .then((value) => {
+    console.log(value);
+  })
+  .catch();
